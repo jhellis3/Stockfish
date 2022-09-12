3d296fa56c822bd0d261ae1d184c14899b07ac92
Joseph Ellis <jhellis3@gmail.com>
2022-09-12 10:28:11 -0500

    Bugfix / TT adjustment
    Bench: 6368560

--- src/tt.cpp
+++ src/tt.cpp
@@ -42,7 +42,7 @@ void TTEntry::save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev) 
   // Overwrite less valuable entries (cheapest checks first)
   if (   b == BOUND_EXACT
       || k != key
-      || d - DEPTH_OFFSET - 2 * pv >= depth8)
+      || d - DEPTH_OFFSET + 3 * pv >= depth8)
   {
       assert(d > DEPTH_OFFSET);
       assert(d < 256 + DEPTH_OFFSET);
